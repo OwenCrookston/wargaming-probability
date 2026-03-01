b26a399de0da7562ef8f60b207d0cf55ab5be0a7
Owen Crookston <owen.crookston@gmail.com>
2026-03-01 14:47:55 -0800

    forcing change

--- src/math/simulate.ts
+++ src/math/simulate.ts
@@ -1,20 +1,20 @@
-import { RollParams, SimulationResult } from './types'
-import { simulateCountSuccesses } from './countSuccesses'
-import { simulateKeepAndSum } from './keepAndSum'
-import { simulateStraightSum } from './straightSum'
+import { RollParams, SimulationResult } from "./types";
+import { simulateCountSuccesses } from "./countSuccesses";
+import { simulateKeepAndSum } from "./keepAndSum";
+import { simulateStraightSum } from "./straightSum";
 
-export const DEFAULT_SAMPLES = 1_000_000
+export const DEFAULT_SAMPLES = 1_000_000;
 
 export function simulate(
-  params: RollParams,
-  samples: number = DEFAULT_SAMPLES,
+    params: RollParams,
+    samples: number = DEFAULT_SAMPLES,
 ): SimulationResult {
-  switch (params.rollType) {
-    case 'countSuccesses':
-      return simulateCountSuccesses(params, samples)
-    case 'keepAndSum':
-      return simulateKeepAndSum(params, samples)
-    case 'straightSum':
-      return simulateStraightSum(params, samples)
-  }
+    switch (params.rollType) {
+        case "countSuccesses":
+            return simulateCountSuccesses(params, samples);
+        case "keepAndSum":
+            return simulateKeepAndSum(params, samples);
+        case "straightSum":
+            return simulateStraightSum(params, samples);
+    }
 }
